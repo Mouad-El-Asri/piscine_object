4f4a62e417af97e7dd2decce05142b659674a4e3
Mouad-El-Asri <elasri.mouad.2002@gmail.com>
2024-07-09 17:08:47 +0100

    ex01: Fix an accidental deletion of code

--- Module_00_Encapsulation/ex01/main.cpp
+++ Module_00_Encapsulation/ex01/main.cpp
@@ -1,10 +1,11 @@
 #include "Graph.hpp"
 
 int main() {
-	Graph graph = Graph(5);
+	Graph graph = Graph(6);
 	graph.addPoint(0, 0);
 	graph.addPoint(2, 2);
 	graph.addPoint(4, 2);
 	graph.addPoint(2, 4);
+	graph.addPoint(5, 5);
 	graph.displayGraph();
 }
